0133d55ef4d0057218614daaef3164060d5d419d
Naveen Mathew <naveenmathew1585@gmail.com>
2023-10-07 11:33:46 +0530

    ✨ basic pub sub working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
 #include <RCSwitch.h>
+#include <WiFi.h>
 #include <config.h>
 
 RCSwitch mySwitch = RCSwitch();
 bool switchRelayFlag = false;
 unsigned long recievedRfKey = 0;
+
+WiFiClient ESPClient;
+PubSubClient client(ESPClient);
 
 void setup() {
   Serial.begin(115200);
@@ -12,7 +18,26 @@ void setup() {
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
 
-  Serial.println("\nAll Systems Ready");
+  // Initialize WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+  if (!WiFi.config(device_local_IP, device_gateway, device_subnet,
+                   device_primaryDNS, device_secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed");
+    while (1) {
+      delay(1000);
+    }
+  } else {
+    Serial.print("ESP IP Address : ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // Initialize MQTT
+  client.setServer(MQTT_Server, MQTT_Port);
+  client.setCallback(MQTT_Subscription_Callback);
 }
 
 void loop() {
@@ -26,6 +51,12 @@ void loop() {
     }
     mySwitch.resetAvailable();
   }
+
+  if (!client.connected()) {
+    MQTT_Reconnect();
+  }
+
+  client.loop();
 
   // Control the relay with appropriate flag
   controlRealy();
@@ -45,11 +76,49 @@ bool isAuthorizedRF(unsigned long keyIdDecimal) {
 void controlRealy() {
   if (switchRelayFlag) {
     Serial.println("[Relay] Switching On Relay");
+    client.publish(MQTT_Publish_Topic, "open");
     digitalWrite(relayPin, HIGH);
     delay(relayOnDelay);
     Serial.println("[Relay] Switching Off Relay");
+    client.publish(MQTT_Publish_Topic, "close");
     switchRelayFlag = false;
   } else {
     digitalWrite(relayPin, LOW);
+  }
+}
+
+void MQTT_Reconnect() {
+  while (!client.connected()) {
+    Serial.print("[MQTT] Attempting MQTT connection...");
+    String clientId = "ESP32Client-";  // Random Client Id
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+
+      client.subscribe(MQTT_Subscription_Topic);
+    } else {
+      Serial.print("[MQTT] failed, rc=");
+      Serial.print(client.state());
+      delay(1000);
+    }
+  }
+}
+
+void MQTT_Subscription_Callback(char* topic, byte* payload,
+                                unsigned int length) {
+  Serial.print("[MQTT] Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  DynamicJsonDocument docSub(1024);
+  deserializeJson(docSub, payload);
+  const char* command = docSub["cmd"];
+  if (strcmp(command, "open") == 0) {
+    switchRelayFlag = true;
   }
 }
